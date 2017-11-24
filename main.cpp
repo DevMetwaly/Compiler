919dcb1e50d8ec2a5d7fd753420c8b159ba704a8
spearkiller0 <abdohamdymetwaly@gmail.com>
2017-11-24 14:53:11 +0200

    Tarek: parse Tree

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@ using namespace std;
 
 vector<string>tokens;
 vector<string>types;
-vector<vector<string> > commands;//bally
-vector<vector<string> > ctypes;//bally
+vector<vector<string> > commands;
+vector<vector<string> > ctypes;
 
 map<string, map <string,vector<string> > >table1;
 
@@ -164,7 +164,7 @@ void build_parser_table(){
     table1["Y"]["&"]=v;
 }
 
-void command_grouping (){ //bally
+void comman_grouping (){
     int j=0;
     while(true){
         vector<string> command;
@@ -176,6 +176,13 @@ void command_grouping (){ //bally
                     j++;
                     break;
 
+            }else if(strcmp(tokens[j].c_str(),"}")==0 || strcmp(tokens[j].c_str(),"{")==0){
+                j++;
+                command.pop_back();
+                type.pop_back();
+                command.push_back(";");
+                type.push_back(";");
+                break;
             }
         }
         commands.push_back(command);
@@ -363,7 +370,7 @@ void DFA(){
     }
 }
 
-void ss(stack<string> s){
+void stack_print(stack<string> s){
     string ss=s.top();
     s.pop();
     while(!s.empty()){
@@ -382,12 +389,17 @@ bool parser(vector<string>Class,vector <string> command ,map<string, map<string,
     cout<<setw(15)<<"STACK"<<setw(25)<<"INPUT"<<setw(25)<<"ACTION\n";
     int i=0;
     while(i<Class.size()){
-        ss(s);
+        stack_print(s);
         if(s.empty()) return false;
         string top=s.top();
         string token=(strcmp(Class[i].c_str(),"keyword")==0|| strcmp(Class[i].c_str(),"operator")==0 ||strcmp(Class[i].c_str(),"paran")==0)? command[i] : Class[i]; //bally
 
-        cout<<setw(25)<<token;
+        string sss=command[i];
+        for(int j=i+1;j<Class.size();j++){
+            sss+=' '+command[j];
+        }
+        cout<<setw(25)<<sss;
+
 
         if(!(isupper(top[0]))){
             if(strcmp(top.c_str(),token.c_str())!=0) {cout<<"###\n";return false;}
@@ -414,6 +426,309 @@ bool parser(vector<string>Class,vector <string> command ,map<string, map<string,
 
 }
 
+class TreeNode
+{
+    private:
+        string token;
+        string type;
+
+        TreeNode *parent;
+
+        vector<TreeNode *> children;
+
+        int countNodesRec(TreeNode *root, int& count){
+            TreeNode *parent = root;
+            TreeNode *child = NULL;
+
+            for(int i = 0; i < parent->childrenNumber(); i++)
+            {
+                if (parent->token=="main") cout<<"--main--"<<endl;
+                child = parent->getChild(i);
+                count++;
+                cout<<child->getToken()<<", "<<child->getType()<<" \t node : "<<count<<endl;
+                if(child->childrenNumber() > 0)
+                {
+                    countNodesRec(child, count);
+                }
+            }
+
+            return count;
+        }
+
+    public:
+        TreeNode(){};
+        TreeNode(string Token, string Type) :
+            token(Token),
+            type(Type),
+            parent(NULL)
+        {}
+
+        void appendChild(TreeNode *child){
+            child->setParent(this);
+            children.push_back(child);
+        }
+
+        void setParent(TreeNode *theParent){
+            parent = theParent;
+        }
+
+        TreeNode* getParent(){
+                return parent;
+        }
+
+        TreeNode* getChild(int pos){
+            if(children.size() < pos)
+                return NULL;
+            else
+                return children[pos];
+        }
+
+
+        int childrenNumber(){
+            return children.size();
+        }
+
+        int treeDFS(){
+            int t = 0;
+
+            if(children.size() < 1)
+            {
+                return 0;
+            }
+
+            countNodesRec(this, t);
+
+            return t;
+        }
+
+        void setToken(string tok){
+            token = tok;
+        }
+
+        void setType(string typ){
+            type = typ;
+        }
+
+        string getToken(){
+            return token;
+        }
+
+        string getType(){
+            return type;
+        }
+};
+
+
+
+int k;
+void expTree(TreeNode *root, vector<string>s){
+    if(k<0) return;
+    TreeNode *node = root;
+
+    if(s[k]=="*" || s[k]=="/" || s[k]=="+" || s[k]=="-" || s[k]=="^"){
+        node->setToken(s[k]);
+        node->setType("operator");
+        k--;
+
+        TreeNode *leftChild = new TreeNode();
+        TreeNode *rightChild = new TreeNode();
+
+        node->appendChild(leftChild);
+        node->appendChild(rightChild);
+
+        expTree(rightChild, s);
+        expTree(leftChild, s);
+    }
+    else if((s[k][0] >= '0' && s[k][0] <= '9')){
+        node->setToken(s[k]); node->setType("number"); k--;
+        return;
+    }
+    else{
+        node->setToken(s[k]); node->setType("variable"); k--;
+        return;
+    }
+}
+
+
+//Function to return precedence of operators
+int prec(char c)
+{
+    if(c == '^')
+    return 3;
+    else if(c == '*' || c == '/')
+    return 2;
+    else if(c == '+' || c == '-')
+    return 1;
+    else
+    return -1;
+}
+// The main function to convert infix expression
+//to postfix expression
+string infixToPostfix(string s)
+{
+    stack<char> st;
+    st.push('N');
+    int l = s.length();
+    string ns;
+    for(int i = 0; i < l; i++)
+    {
+        // If the scanned character is an operand, add it to output string.
+        if((s[i] >= 'a' && s[i] <= 'z')||(s[i] >= 'A' && s[i] <= 'Z')||(s[i] >= '0' && s[i] <= '9'))
+            {
+                while(i<l && ((s[i] >= 'a' && s[i] <= 'z')||(s[i] >= 'A' && s[i] <= 'Z')||(s[i] >= '0' && s[i] <= '9')) ){
+                        ns+=s[i];
+                        i++;
+                }
+                if(i<l) i--;
+                ns+=" ";
+            }
+
+
+        // If the scanned character is an ‘(‘, push it to the stack.
+        else if(s[i] == '(')
+
+        st.push('(');
+
+        // If the scanned character is an ‘)’, pop and to output string from the stack
+        // until an ‘(‘ is encountered.
+        else if(s[i] == ')')
+        {
+            while(st.top() != 'N' && st.top() != '(')
+            {
+                char c = st.top();
+                st.pop();
+               ns += c;
+               ns += " ";
+            }
+            if(st.top() == '(')
+            {
+                char c = st.top();
+                st.pop();
+            }
+        }
+
+        //If an operator is scanned
+        else{
+            while(st.top() != 'N' && prec(s[i]) <= prec(st.top()))
+            {
+                char c = st.top();
+                st.pop();
+                ns += c;
+                ns += " ";
+            }
+            st.push(s[i]);
+        }
+    }
+    //Pop all the remaining elements from the stack
+    while(st.top() != 'N')
+    {
+        char c = st.top();
+        st.pop();
+        ns += c;
+        ns += " ";
+    }
+
+    return ns;
+
+}
+
+TreeNode *infixHandle(string ss){
+
+    string s = ss;
+    string exp = infixToPostfix(s);
+    vector<string>postfix;
+    for(int i=0; i<exp.length(); i++){
+        string x;
+        while(i<exp.length() && exp[i]!=' ')
+            { x+=exp[i]; i++; }
+        postfix.push_back(x);
+    }
+    k=postfix.size()-1;
+    TreeNode *postfixTree=new TreeNode();
+    //TreeNode *child=new TreeNode(); postfixTree->appendChild(child);
+    expTree(postfixTree, postfix);
+    return postfixTree;
+}
+
+
+
+int currentTok=0;
+void generateTree(TreeNode *parent){
+
+    TreeNode *node = parent;
+    string nodeType = node->getType();
+    string nodeVal = node->getToken();
+
+    while(currentTok<tokens.size()){
+
+        if(tokens[currentTok]=="}") {
+                //cout<<types[currentTok]<<currentTok<<endl;
+                currentTok++;
+                return;
+        }
+
+        if(nodeType=="block"){
+            TreeNode *child = new TreeNode();
+            node->appendChild(child);
+            generateTree(child);
+            continue;
+        }
+
+        if(tokens[currentTok]=="int" || tokens[currentTok]=="float"){
+
+            node->setToken(tokens[currentTok]) ; node->setType("datatype") ;
+            currentTok++;
+
+            TreeNode *var = new TreeNode(tokens[currentTok],"identifier");
+            currentTok++;
+            node->appendChild(var);
+
+            if(tokens[currentTok]!=";"){
+                currentTok++; //Skip assign
+                string s;
+                while(tokens[currentTok]!=";"){
+                    s+=tokens[currentTok];
+                    currentTok++;
+                }
+                TreeNode *val = infixHandle(s);
+                node->appendChild(val);
+            }
+            currentTok++; //Skip separator
+
+            return;
+        }
+
+        if(types[currentTok]=="keyword"){
+
+            //Should be changed to Loop until operator, fix them
+            node->setToken(tokens[currentTok]) ; node->setType("keyword") ;
+
+            currentTok+=3;
+
+            TreeNode *op = new TreeNode(tokens[currentTok],"operator");
+            currentTok--;
+            TreeNode *opLeft = new TreeNode(tokens[currentTok],types[currentTok]);
+            currentTok+=2;
+            TreeNode *opRight = new TreeNode(tokens[currentTok],types[currentTok]);
+
+            op->appendChild(opLeft);
+            op->appendChild(opRight);
+            node->appendChild(op);
+
+            currentTok+=2;
+
+            if(tokens[currentTok]=="{"){
+
+                TreeNode *block = new TreeNode("code", "block");
+                node->appendChild(block);
+                currentTok++;
+                generateTree(block);
+            }
+            return;
+        }
+    }
+}
+
 int main(){
     string line;
     ifstream file;
@@ -427,25 +742,48 @@ int main(){
         cout<<tokens[i]<<"\t \t"<<types[i]<<endl;
     }
 
-    command_grouping();
+    comman_grouping();
 
     build_parser_table();
+
+    cout<<"\n--- CODE INSTRUCTIONS ---\n\n";
 
     for(int i=0;i<commands.size();i++){
         for(int j=0;j<commands[i].size();j++)
             cout<<commands[i][j]<<" ";
         cout<<endl;
     }
+
+    cout<<"\n--- CODE CLASSIFIED ---\n\n";
+
     for(int i=0;i<commands.size();i++){
         for(int j=0;j<commands[i].size();j++)
             cout<<ctypes[i][j]<<" ";
         cout<<endl;
     }
+
+    cout<<"\n---ALGORITHM PREVIEW---\n\n";
+
     for(int i=0;i<commands.size();i++){
         if(parser(ctypes[i],commands[i],table1)) cout<<"success 1\n";
         else cout<<"Fail\n";
         cout<<"\n\n\n";
     }
     file.close();
+
+    tokens.clear();
+    types.clear();
+    file.open("mycode1.gh");
+    while(getline(file,line)){
+        line+=" ";
+        separation(line);
+    }
+    DFA();
+    file.close();
+
+    cout<<"\n\n----TREE DFS----\n\n";
+    TreeNode *root = new TreeNode("main", "block");
+    generateTree(root);
+    cout<<root->treeDFS();
     return 0;
 }
